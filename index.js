0f2b039a96e6dc1c0a924c996412c1a518e8d2dd
Noel Mbuya <mbuyanoel@gmail.com>
2021-11-11 17:43:03 +0300

    Add file props for further edit

---
+++ index.js
@@ -0,0 +1,29 @@
+
+
+
+/**
+ * Fetch list of transactions from the Stellar network.
+ */
+ var server = new StellarSdk.Server("https://horizon.stellar.org");
+​
+ var handleFetchTransactions = () => {
+     console.log("Fetching transactions...");
+     let trxList = document.getElementById("transactions");
+     server
+         .transactions()
+         .call()
+         .then(function (resp) {
+             let transactions = resp.records;
+             console.log(transactions);
+            transactions.map(transaction => {
+                trxList.innerHTML += `<li>${transaction.id}</li>`;
+            })
+ 
+         })
+         .catch(function (err) {
+             console.error(err);
+         });
+     
+ }
+ 
+
